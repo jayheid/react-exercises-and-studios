df501aa1831088134ba68ae80e360551cae72cc9
unknown <jp.heidbrink@gmail.com>
2024-08-18 15:22:14 -0500

    Modified Part 1 of Exercise

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+  let pageTitle = "Book List";
+  let book1 = "https://m.media-amazon.com/images/I/61WvKcckCnL._SL1200_.jpg";
+  let book2 = "https://m.media-amazon.com/images/I/61b03PRisyL._SL1360_.jpg";
+  let book3 = "https://m.media-amazon.com/images/I/71+kLdUcDLL._SL1500_.jpg";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+  return (
+    <div>
+      <h3>{pageTitle}</h3>
+      <img src={book1} alt="The Power Broker" />
+      <img src={book2} alt="Professional JavaScript for Web Developers" />
+      <img src={book3} alt="JavaScript: Absolute Beginner's Guide" />
+    </div>
+  );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+   <div>
+      <h3 className = {classes.choresHeading}>Chores Remaining</h3>
+      <ol>
+         <li className = {classes.choresText}>Clean Dishes</li>
+         <li className = {classes.choresText}>Sweep Floor</li>
+         <li className = {classes.choresText}>Walk Dogs</li>
+      </ol>
+   </div>
+   );
 }
