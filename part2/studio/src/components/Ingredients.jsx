a3d1a3d37c93b9f6c393b34a992417b05b4985f6
unknown <jp.heidbrink@gmail.com>
2024-09-01 18:42:57 -0500

    modified studio

--- part2/studio/src/components/Ingredients.jsx
+++ part2/studio/src/components/Ingredients.jsx
@@ -1,9 +1,18 @@
+import recipeData from "./recipe.json";
+
 function IngredientList() {
-   return;
- }
- 
- export default IngredientList;
- 
+  let ingredientJSX = recipeData[0].ingredients.map((ingredient, index) => {
+    return <li key={index}>{ingredient}</li>;
+  });
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      <ul>{ingredientJSX}</ul>
+    </div>
+  );
+}
+
+export default IngredientList;
+
 //import json file for the data
 //use a nested map to get inside the inner array
- 
